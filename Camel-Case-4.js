cf4d97ffe6ae95d17f63d861f644ec2a9fcde727
afajardo-sj <andrezffajardo@gmail.com>
2022-01-18 21:54:26 -0500

    condition case of a combine operation, method and variable with return

--- Camel-Case-4.js
+++ Camel-Case-4.js
@@ -1,20 +1,19 @@
-let string = 'C;M;white sheet of paper';
+let input = 'C;M;white sheet of paper';
 let i=0;
 let character='';
 let b = " ";
 let c = "";
 
-if (string.includes('S;')) {
-let str = string.slice(4).replace('()', '');
+if (input.includes('S;')) {
+let str = input.slice(4).replace('()', '');
 c = str;
 let x = str.length;
 
-while (i <= x){
+while (i < x){
     character = c.charAt(i);
         if (character == character.toUpperCase() && (i>1)) {
             var output = [c.slice(0, i), b, c.slice(i)].join('');
             c = output;
-            console.log(c);
             x++;
             i++;
         }
@@ -22,32 +21,36 @@ while (i <= x){
 }
 
 c = c.toLowerCase();
+return c;
 }
 
-else if (string.includes('C;C;')) {
-  const y = string.slice(4).split(" ");
+else if (input.includes('C;C;')) {
+  const y = input.slice(4).split(" ");
   //console.log("yes1");
   for (let i = 0; i < y.length; i++){
     y[i] =y[i].charAt(0).toUpperCase() + y[i].slice(1);
   }
 
 const str2 = y.join('');
-console.log(str2);
+return str2;
+//console.log(str2);
 }
 
-  else if (string.includes('C;V;')) {
-  const y = string.slice(4).split(" ");
+  else if (input.includes('C;V;')) {
+  const y = input.slice(4).split(" ");
     y[1] = y[1].charAt(0).toUpperCase() + y[1].slice(1);
 const str2 = y.join('');
-console.log(str2);
+return str2;
+//console.log(str2);
   }
 
-    else if (string.includes('C;M;')) {
-      const y = string.slice(4).split(" ");
+    else if (input.includes('C;M;')) {
+      const y = input.slice(4).split(" ");
   for (let i = 0; i < y.length; i++){
     y[i] =y[i].charAt(0).toUpperCase() + y[i].slice(1);
   }
 
 const str2 = y.join('') + "()";
-console.log(str2);
+return str2;
+//console.log(str2);
 }
